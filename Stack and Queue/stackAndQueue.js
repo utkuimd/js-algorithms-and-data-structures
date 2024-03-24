1ce5f02f85218d4d69de2b3f8baf238ee3ef4b75
Utku İmdat <ut.imd@yandex.com>
2024-03-24 16:31:05 +0300

    Added stack data structure

---
+++ Stack and Queue/stackAndQueue.js
@@ -0,0 +1,46 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+    // Works
+    push(val) {
+        var newNode = new Node(val);
+        if(this.size === 0) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            // F        
+            // 98 -> 99 100 101
+            var temp = this.first;
+            this.first = newNode;
+            this.first.next = temp;
+        }
+        return ++this.size;
+    }
+    // Works
+    pop() {
+        if(!this.first) {
+            return null;
+        }
+        var temp = this.first;
+        if(this.first === this.last) {
+            this.last = null;
+        }
+        // T     F   
+        // 98 -> 99 100 101
+        this.first = this.first.next;
+        this.size--;
+        return temp.value;
+    }
+}
+
+var stack = new Stack();
