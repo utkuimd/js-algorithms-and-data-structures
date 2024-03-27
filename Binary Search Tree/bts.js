cea963262269959c1186b482a74fe728902250cb
Utku İmdat <ut.imd@yandex.com>
2024-03-27 17:27:31 +0300

    Added BFS and DFS_Pre_Order

--- Binary Search Tree/bts.js
+++ Binary Search Tree/bts.js
@@ -58,6 +58,39 @@ class BTS {
         };
         return current;
     }
+    // Works
+    BFS() {
+        var node = this.root, data = [], queue = [];
+        queue.push(node);
+
+        while(queue.length) {
+            node = queue.shift();
+            data.push(node.value);
+            if(node.left) {
+                queue.push(node.left);
+            }
+            if(node.right) {
+                queue.push(node.right);
+            }
+        }
+        return data;
+    }
+    // Works
+    DFS_Pre_Order() {
+        var data = [];
+        var current = this.root;
+        function traverse(node) {
+            data.push(node.value);
+            if(node.left) {
+                traverse(node.left);
+            }
+            if(node.right) {
+                traverse(node.right);
+            }
+        }
+        traverse(current);
+        return data;
+    }
 }
 //          10
 //      5       13
